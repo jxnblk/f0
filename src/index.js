b6302fd6fa347ae62272073687f140ce48c15793
jxnblk <jacksonblack@gmail.com>
2016-10-23 13:57:26 -0400

    Add index and build script

---
+++ src/index.js
@@ -0,0 +1,18 @@
+
+import Line from './Line'
+import Bars from './Bars'
+import Group from './Group'
+import withScale from './withScale'
+
+export { default as Line } from './Line'
+export { default as Bars } from './Bars'
+export { default as Group } from './Group'
+export { default as withScale } from './withScale'
+
+export default {
+  Line,
+  Bars,
+  Group,
+  withScale
+}
+
